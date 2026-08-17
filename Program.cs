f91acbb7ae2efa997cb48d5bd0b93910830f19f7
Tima-simati <timaaks0ul@gmail.com>
2026-08-17 17:55:58 +0200

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace QuizMaker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Question firstQuesiton = new Question();
+            firstQuesiton.question = "What is the highest mountain in the world?";
+            firstQuesiton.possibleOptions.Add("K2");
+            firstQuesiton.possibleOptions.Add("Mount Everest");
+            firstQuesiton.possibleOptions.Add("Mount Blanc");
+            firstQuesiton.possibleOptions.Add("Zugspitze");
+
+            Console.WriteLine(firstQuesiton.question);
+            foreach (string i in firstQuesiton.possibleOptions)
+            {
+                Console.Write($"{i} ");
+            }
+        }
+    }
+}
---
+++ Question.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuizMaker
+{
+    internal class Question
+    {
+        public string question;
+        public List<string> possibleOptions = new List<string>();
+        public List<string> answer = new List<string>(); //answer stored in List to support multiple choice questions
+    }
+}
