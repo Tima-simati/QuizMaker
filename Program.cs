391d87ed874cf46e0236fe974bed8b725e4626e4
Tima-simati <timaaks0ul@gmail.com>
2026-08-18 16:10:37 +0200

    added class QuizStorage for XML serialization and deserialization

--- Program.cs
+++ Program.cs
@@ -4,19 +4,35 @@
     {
         static void Main(string[] args)
         {
+            var QuizList = new List<Question>();    
+
             Question firstQuesiton = new Question();
             firstQuesiton.question = "What is the highest mountain in the world?";
             firstQuesiton.possibleOptions.Add("K2");
             firstQuesiton.possibleOptions.Add("Mount Everest");
             firstQuesiton.possibleOptions.Add("Mount Blanc");
             firstQuesiton.possibleOptions.Add("Zugspitze");
+            firstQuesiton.answer.Add("Mount Everest");
+
+            Question secondQuesiton = new Question();
+            secondQuesiton.question = "What is longest river in the world?";
+            secondQuesiton.possibleOptions.Add("Nile");
+            secondQuesiton.possibleOptions.Add("Amazonas");
+            secondQuesiton.possibleOptions.Add("Danube");
+            secondQuesiton.possibleOptions.Add("Wolga");
+            secondQuesiton.answer.Add("Nile");
 
             Console.WriteLine(firstQuesiton.question);
             foreach (string i in firstQuesiton.possibleOptions)
             {
-                Console.Write($"{i} ");
+                Console.WriteLine($"{i} ");
             }
-            Question secondQuesiton = new Question();
+
+            //List created to store all question
+            QuizList.Add(firstQuesiton);
+            QuizList.Add(secondQuesiton);
+
+            QuizStorage.StoreQuestion(QuizList);
         }
     }
 }
---
+++ QuizStorage.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
+using System.IO;
+
+
+namespace QuizMaker
+{
+    internal class QuizStorage
+    {
+        public static XmlSerializer serializer = new XmlSerializer(typeof(List<Question>));
+
+        public static void StoreQuestion(List<Question> quizlist)
+        {
+            var path = @"C:\Users\Timur\source\repos\QuizMaker\QuizStorage\QuizDataBase.xml";
+            using (FileStream file = File.Create(path))
+            {
+                serializer.Serialize(file, quizlist);
+            }
+        }
+        public static List<Question> ReadQuestion()
+        {
+            var path = @"C:\Users\Timur\source\repos\QuizMaker\QuizStorage\QuizDataBase.xml";
+            using (FileStream file = File.OpenRead(path))
+            {                
+               return serializer.Deserialize(file) as List<Question>;
+            }
+        }
+    }
+}
